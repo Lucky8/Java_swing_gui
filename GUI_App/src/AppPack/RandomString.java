43eb5ebeda2c19af71cca8130de827251f448bca
Lucky Ndlovu <Lucky8@users.noreply.github.com>
2019-08-11 03:44:15 +0200

    Add files via upload

---
+++ GUI_App/src/AppPack/RandomString.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2018, Ltech Dev. All rights reserved.
+ * Ltech Dev 2018.
+ * Developer - Lucky Ndlovu.
+ */
+package AppPack;
+
+/**
+ *
+ * @author Lucky
+ */
+public class RandomString {
+    
+    // function to generate a random string of length n 
+    static String getAlphaNumericString(int n) 
+    { 
+  
+        // chose a Character random from this String 
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                    + "0123456789"
+                                    + "abcdefghijklmnopqrstuvxyz"; 
+  
+        // create StringBuffer size of AlphaNumericString 
+        StringBuilder sb = new StringBuilder(n); 
+        
+        for (int i = 0; i < n; i++) { 
+  
+            // generate a random number between 
+            // 0 to AlphaNumericString variable length 
+            int index 
+                = (int)(AlphaNumericString.length() 
+                        * Math.random()); 
+  
+            // add Character one by one in end of sb 
+            sb.append(AlphaNumericString 
+                          .charAt(index)); 
+        } 
+  
+        return sb.toString(); 
+    }  
+   
+}
